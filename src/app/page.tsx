259060cc97011f0cb2f4641a43f5377ef5e6da82
ahmad <itxti909@gmail.com>
2024-04-21 16:34:10 +0300

    req

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { Features } from "@/components/Features";
+import { Faq } from "@/components/Feq";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-   <>
-    <Navbar/>
-    <Hero/>
-    <Features/>
-   </>
+    <>
+      <Navbar />
+      <Hero />
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+        <Features />
+        <Faq />
+        {/* <Pricing />
+        <Cta />
+        <Footer /> */}
+      </div>
+    </>
   );
 }
